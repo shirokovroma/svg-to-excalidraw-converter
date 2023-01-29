4e97449a82cdc3014e649ed8f060655c5cdd4bc9
Roman Shirokov <shirokovroma@gmail.com>
2023-01-30 00:19:24 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,47 @@
+import svgToEx from "svg-to-excalidraw";
+
+const fileSelector = document.getElementById("source");
+const textContainer = document.getElementById("text");
+fileSelector.addEventListener("change", (event) => {
+  textContainer.innerText = "Processing...";
+  const fileList = event.target.files;
+  for (let i = 0; i < fileList.length; i++) {
+    const name = fileList[i].name.replace(".svg", ".excalidraw");
+    const contents = readFile(fileList[i]);
+    downloadFile(name, contents);
+  }
+  // clear the input
+  event.target.value = "";
+  textContainer.innerHTML = "Drag and drop your svg file here <br>or click to select file";
+});
+
+function readFile(file) {
+  if (file.type && file.type !== "image/svg+xml") {
+    console.log("File is not SVG.");
+
+    return;
+  }
+
+  const reader = new FileReader();
+
+  reader.readAsText(file);
+  reader.addEventListener("load", (event) => {
+   const { hasErrors, errors, content } = svgToEx.convert(event.target.result);
+   if (hasErrors) {
+     console.log(errors);
+   }
+   console.log(content);
+   return JSON.stringify(content, null, 2)
+  });
+}
+
+function downloadFile(filename, contents){
+  const link = document.createElement('a');
+  link.download = filename;
+  link.href = 'data:text/plain;charset=utf-8,' + encodeURIComponent(contents);
+  document.body.appendChild(link);
+  link.click();
+
+  document.body.removeChild(link);
+  return link;
+}
